b3b640279e958f5a78efe57850cf0a9e8a7ffd19
Hackerl <490021209@qq.com>
2020-11-29 18:52:11 +0800

    Add#warning log level

--- log.h
+++ log.h
@@ -8,8 +8,9 @@
 
 #define NO_LOG          0x00
 #define ERROR_LEVEL     0x01
-#define INFO_LEVEL      0x02
-#define DEBUG_LEVEL     0x03
+#define WARNING_LEVEL   0x02
+#define INFO_LEVEL      0x03
+#define DEBUG_LEVEL     0x04
 
 #ifndef LOG_LEVEL
 #define LOG_LEVEL   DEBUG_LEVEL
@@ -17,12 +18,13 @@
 
 #define PRINT_FUNCTION(format, ...)      fprintf(stderr, format, __VA_ARGS__)
 
-#define LOG_FMT             "%s | %-5s | %20s:%-4d] "
+#define LOG_FMT             "%s | %-7s | %20s:%-4d] "
 #define LOG_ARGS(LOG_TAG)   CTimeHelper::getTimeString().c_str(), LOG_TAG, _FILE, __LINE__
 
 #define NEWLINE     "\n"
 
 #define ERROR_TAG   "ERROR"
+#define WARNING_TAG "WARNING"
 #define INFO_TAG    "INFO"
 #define DEBUG_TAG   "DEBUG"
 
@@ -36,6 +38,12 @@
 #define LOG_INFO(message, args...)      PRINT_FUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(INFO_TAG), ## args)
 #else
 #define LOG_INFO(message, args...)
+#endif
+
+#if LOG_LEVEL >= WARNING_LEVEL
+#define LOG_WARNING(message, args...)   PRINT_FUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(WARNING_TAG), ## args)
+#else
+#define LOG_WARNING(message, args...)
 #endif
 
 #if LOG_LEVEL >= ERROR_LEVEL

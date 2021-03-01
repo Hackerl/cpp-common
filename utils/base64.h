fb04b28496ba88d85546fe84a70fa933f2426d3f
Hackerl <490021209@qq.com>
2021-03-01 12:43:55 +0800

    Change#base64/binascii utils

--- utils/base64.h
+++ utils/base64.h
@@ -4,19 +4,20 @@
 #include <vector>
 #include <string>
 
-constexpr unsigned char from_base64[] = {
+constexpr unsigned char BASE64_FROM_MAP[] = {
         255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
         255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,  62, 255,  62, 255,  63,
-        52,  53,  54,  55,  56,  57,  58,  59,  60,  61, 255, 255, 255, 255, 255, 255,
-        255,   0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,
-        15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25, 255, 255, 255, 255,  63,
-        255,  26,  27,  28,  29,  30,  31,  32,  33,  34,  35,  36,  37,  38,  39,  40,
-        41,  42,  43,  44,  45,  46,  47,  48,  49,  50,  51, 255, 255, 255, 255, 255 };
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 62,  255, 62,  255, 63,
+        52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  255, 255, 255, 255, 255, 255,
+        255, 0,   1,   2,   3,   4,   5,   6,   7,   8,   9,   10,  11,  12,  13,  14,
+        15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  255, 255, 255, 255, 63,
+        255, 26,  27,  28,  29,  30,  31,  32,  33,  34,  35,  36,  37,  38,  39,  40,
+        41,  42,  43,  44,  45,  46,  47,  48,  49,  50,  51,  255, 255, 255, 255, 255
+};
 
-constexpr char to_base64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                             "abcdefghijklmnopqrstuvwxyz"
-                             "0123456789+/";
+constexpr char BASE64_TO_MAP[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                 "abcdefghijklmnopqrstuvwxyz"
+                                 "0123456789+/";
 
 class CBase64 {
 public:
@@ -27,76 +28,81 @@ public:
         return encode(buffer.data(), buffer.size());
     }
 
-    static std::string encode(const unsigned char *buf, size_t bufLen) {
+    static std::string encode(const unsigned char *buffer, size_t length) {
         size_t missing = 0;
-        size_t ret_size = bufLen;
+        size_t size = length;
 
-        while ((ret_size % 3) != 0) {
-            ret_size++;
+        while ((size % 3) != 0) {
+            size++;
             missing++;
         }
 
-        ret_size = 4 * ret_size / 3;
+        size = 4 * size / 3;
 
-        std::string ret;
-        ret.reserve(ret_size);
+        std::string encoded;
+        encoded.reserve(size);
 
-        for (unsigned int i = 0; i < ret_size / 4; i++) {
+        for (unsigned int i = 0; i < size / 4; i++) {
             size_t index = i * 3;
-            unsigned char b3[3];
+            unsigned char b3[3] = {};
 
-            b3[0] = (index + 0 < bufLen) ? buf[index + 0] : 0;
-            b3[1] = (index + 1 < bufLen) ? buf[index + 1] : 0;
-            b3[2] = (index + 2 < bufLen) ? buf[index + 2] : 0;
+            b3[0] = (index + 0 < length) ? buffer[index + 0] : 0;
+            b3[1] = (index + 1 < length) ? buffer[index + 1] : 0;
+            b3[2] = (index + 2 < length) ? buffer[index + 2] : 0;
 
-            unsigned char b4[4];
+            unsigned char b4[4] = {};
 
             b4[0] = ((b3[0] & 0xfcU) >> 2U);
             b4[1] = ((b3[0] & 0x03U) << 4U) + ((b3[1] & 0xf0U) >> 4U);
             b4[2] = ((b3[1] & 0x0fU) << 2U) + ((b3[2] & 0xc0U) >> 6U);
             b4[3] = ((b3[2] & 0x3fU) << 0U);
 
-            ret.push_back(to_base64[b4[0]]);
-            ret.push_back(to_base64[b4[1]]);
-            ret.push_back(to_base64[b4[2]]);
-            ret.push_back(to_base64[b4[3]]);
+            encoded.push_back(BASE64_TO_MAP[b4[0]]);
+            encoded.push_back(BASE64_TO_MAP[b4[1]]);
+            encoded.push_back(BASE64_TO_MAP[b4[2]]);
+            encoded.push_back(BASE64_TO_MAP[b4[3]]);
         }
 
         for (size_t i = 0; i < missing; i++)
-            ret[ret_size - i - 1] = '=';
+            encoded[size - i - 1] = '=';
 
-        return ret;
+        return encoded;
     }
 
-    static std::vector<unsigned char> decode(std::string encoded_string) {
-        while ((encoded_string.size() % 4) != 0)
-            encoded_string.push_back('=');
+    static std::vector<unsigned char> decode(std::string encoded) {
+        while ((encoded.size() % 4) != 0)
+            encoded.push_back('=');
 
-        size_t encoded_size = encoded_string.size();
+        size_t size = encoded.size();
 
-        std::vector<unsigned char> ret;
-        ret.reserve(3 * encoded_size / 4);
+        std::vector<unsigned char> buffer;
+        buffer.reserve(3 * size / 4);
 
-        for (size_t i = 0; i < encoded_size; i += 4) {
-            unsigned char b4[4];
+        for (size_t i = 0; i < size; i += 4) {
+            unsigned char b4[4] = {};
 
-            b4[0] = (encoded_string[i + 0] <= 'z') ? from_base64[encoded_string[i + 0]] : 0xff;
-            b4[1] = (encoded_string[i + 1] <= 'z') ? from_base64[encoded_string[i + 1]] : 0xff;
-            b4[2] = (encoded_string[i + 2] <= 'z') ? from_base64[encoded_string[i + 2]] : 0xff;
-            b4[3] = (encoded_string[i + 3] <= 'z') ? from_base64[encoded_string[i + 3]] : 0xff;
+            b4[0] = (encoded[i + 0] <= 'z') ? BASE64_FROM_MAP[encoded[i + 0]] : 0xff;
+            b4[1] = (encoded[i + 1] <= 'z') ? BASE64_FROM_MAP[encoded[i + 1]] : 0xff;
+            b4[2] = (encoded[i + 2] <= 'z') ? BASE64_FROM_MAP[encoded[i + 2]] : 0xff;
+            b4[3] = (encoded[i + 3] <= 'z') ? BASE64_FROM_MAP[encoded[i + 3]] : 0xff;
 
-            unsigned char b3[3];
+            unsigned char b3[3] = {};
 
             b3[0] = ((b4[0] & 0x3fU) << 2U) + ((b4[1] & 0x30U) >> 4U);
             b3[1] = ((b4[1] & 0x0fU) << 4U) + ((b4[2] & 0x3cU) >> 2U);
             b3[2] = ((b4[2] & 0x03U) << 6U) + ((b4[3] & 0x3fU) >> 0U);
 
-            if (b4[1] != 0xff) ret.push_back(b3[0]);
-            if (b4[2] != 0xff) ret.push_back(b3[1]);
-            if (b4[3] != 0xff) ret.push_back(b3[2]);
+            if (b4[1] != 0xff)
+                buffer.push_back(b3[0]);
+
+            if (b4[2] != 0xff)
+                buffer.push_back(b3[1]);
+
+            if (b4[3] != 0xff)
+                buffer.push_back(b3[2]);
         }
 
-        return ret;
+        return buffer;
     }
 };
 //******************************************************************************
--- utils/binascii.h
+++ utils/binascii.h
@@ -1,35 +1,38 @@
 #ifndef __Binascii_H__
 #define __Binascii_H__
 //******************************************************************************
+#include "string_helper.h"
 #include <vector>
 #include <sstream>
 
-constexpr char hexMap[] = {'0', '1', '2', '3', '4', '5', '6', '7',
-                           '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-class CBinascii
-{
+constexpr char HEX_MAP[] = {
+        '0', '1', '2', '3', '4', '5', '6', '7',
+        '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+};
+
+class CBinascii {
 public:
     static std::string hexlify(const unsigned char *buffer, unsigned long length) {
         std::string s(length * 2, ' ');
 
-        for (int i = 0; i < length; ++i) {
-            s[2 * i] = hexMap[(buffer[i] & 0xF0U) >> 4U];
-            s[2 * i + 1] = hexMap[buffer[i] & 0x0FU];
+        for (int i = 0; i < length; i++) {
+            s[2 * i] = HEX_MAP[(buffer[i] & 0xF0U) >> 4U];
+            s[2 * i + 1] = HEX_MAP[buffer[i] & 0x0FU];
         }
 
         return s;
     }
 
-    static std::vector<unsigned char> unHexlify(const std::string& hexString) {
+    static std::vector<unsigned char> unHexlify(const std::string &hex) {
         std::vector<unsigned char> buffer;
 
-        for (unsigned long i = 0; i < hexString.size(); i += 2) {
+        for (unsigned long i = 0; i < hex.size(); i += 2) {
             unsigned int number;
 
-            std::istringstream ss(hexString.substr(i, 2));
-            ss >> std::hex >> number;
+            if (!CStringHelper::toNumber(hex.substr(i, 2), number, 16))
+                break;
 
-            buffer.push_back(number & 0xffU);
+            buffer.push_back(number & 0xFFU);
         }
 
         return buffer;

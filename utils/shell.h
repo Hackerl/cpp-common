3d06b06d6680dc719bc64ff6c29a93a615f39aef
Hackerl <490021209@qq.com>
2021-03-04 10:52:54 +0800

    Add#file match shell api

--- utils/shell.h
+++ utils/shell.h
@@ -3,18 +3,39 @@
 //******************************************************************************
 #include <list>
 #include <string>
+#include <glob.h>
 #include <wordexp.h>
 //******************************************************************************
 class CShellAPI {
 public:
+    static bool match(const std::string &pattern, std::list<std::string> &paths) {
+        glob_t g = {};
+
+        if (glob(pattern.c_str(), 0, nullptr, &g) != 0) {
+            globfree(&g);
+            return false;
+        }
+
+        for (int i = 0; i < g.gl_pathc; i++) {
+            paths.emplace_back(g.gl_pathv[i]);
+        }
+
+        globfree(&g);
+
+        return true;
+    }
+
     static bool expansion(const std::string &str, std::list<std::string> &words) {
         wordexp_t p = {};
 
-        if (wordexp(str.c_str(), &p, WRDE_NOCMD) != 0)
+        if (wordexp(str.c_str(), &p, WRDE_NOCMD) != 0) {
+            wordfree(&p);
             return false;
+        }
 
-        for (int i = 0; i < p.we_wordc; i++)
+        for (int i = 0; i < p.we_wordc; i++) {
             words.emplace_back(p.we_wordv[i]);
+        }
 
         wordfree(&p);
 
